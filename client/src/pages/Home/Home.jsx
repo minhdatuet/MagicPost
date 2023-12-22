6000c17fbfa2b32c4e19585e46791332807cbd2e
weiwei146 <100007345+weiwei146@users.noreply.github.com>
2023-12-23 00:38:28 +0700

    Search Package ID at home for customer

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,112 +1,134 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import React from 'react';
 import './Home.css';
 import { apiGetPackage, apiGetUser } from '../../services/user'
 import { useSelector, useDispatch } from 'react-redux'
-import * as actions from '../../store/actions'
-import { apiSearch } from '../../services/auth';
+import { getAllPackages } from "../../store/actions/package";
 import tem from '../../assets/images/tem.png'
 
 
 export const Home = () => {
-
+  const dispatch = useDispatch();
   const [packageID, setPackageID] = useState('');
-  const [packageInfo, setPackageInfo] = useState(null);
+  const { packages } = useSelector((state) => state.package);
+  const [showPackageInfo, setShowPackageInfo] = useState(false);
+  const [packageItem, setSearchedPackage] = useState(null);
+  
+
+  useEffect(() => {
+    dispatch(getAllPackages());
+  }, []);
 
   const handleSearch = async () => {
-    try {
-      const response = await apiSearch(packageID)
-      setPackageInfo(response);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+    const foundPackage = packages.find(packageItem => packageItem.id === Number(packageID));
+    setSearchedPackage(foundPackage);
+    setShowPackageInfo(!!foundPackage);
   };
-  
   return (
-    <div id = "home">
-      <div className = "flexSearch">
-        <input type = "text" 
-        id = "searchInformation-input" 
-        placeholder='Nhập mã đơn hàng...'
-        value={packageID}
-        onChange={(e) => setPackageID(e.target.value)}>
+    <div id="home">
+      <div className="flexSearch">
+        <input type="text"
+          id="searchInformation-input"
+          placeholder='Nhập mã đơn hàng...'
+          value={packageID}
+          onChange={(e) => setPackageID(e.target.value)}>
         </input>
-        <button id = "btnSubmit" onClick={handleSearch}>
+        <button id="btnSubmit" onClick={handleSearch}>
           Xác nhận
         </button>
       </div>
-
-      <div className='packageForm'>
-        <div className = "flex1"></div>
-        <div className = "flex2">
+      {showPackageInfo && packageItem && (
+        <div className='packageForm'>
+        <div className="flex1"></div>
+        <div className="flex2">
           <h1>BIỂU MẪU ĐƠN HÀNG</h1>
           <p>Magic Post</p>
         </div>
         <div className="flex3">
           <div className='info'>
             <div className='flex3-1'>
-              <div className = "flex1">
+              <div className="flex1">
                 <p>THÔNG TIN NGƯỜI DÙNG</p>
               </div>
-              <div className = "flex4">
-                <div className = "flex5">
+              <div className="flex4">
+                <div className="flex5">
+                  <p className='pLabel'><strong>(*) Người gửi</strong></p>
+                </div>
+                <div className="flex5">
                   <p className='pLabel'>Họ và tên: </p>
-                  <p>Ngô Yến Vi</p>
+                  <p>{packageItem.sender.name}</p>
                 </div>
-                <div className = "flex5">
+                <div className="flex5">
                   <p className='pLabel'>Số điện thoại: </p>
-                  <p>0347418330</p>
+                  <p>{packageItem.sender.phone}</p>
                 </div>
-                <div className = "flex5">
+                <div className="flex5">
                   <p className='pLabel'>Địa chỉ: </p>
-                  <p>Hồ Tùng Mậu, Cầu Diễn, Hà Nội</p>
+                  <p>{packageItem.sender.address}</p>
                 </div>
-                <img id ="tem" src = {tem} alt = ""></img>
+              </div>
+              <hr></hr>
+              <div className="flex4">
+                <div className="flex5">
+                  <p className='pLabel'><strong>(*) Người nhận</strong></p>
+                </div>
+                <div className="flex5">
+                  <p className='pLabel'>Họ và tên: </p>
+                  <p>{packageItem.receiver.name}</p>
+                </div>
+                <div className="flex5">
+                  <p className='pLabel'>Số điện thoại: </p>
+                  <p>{packageItem.receiver.phone}</p>
+                </div>
+                <div className="flex5">
+                  <p className='pLabel'>Địa chỉ: </p>
+                  <p>{packageItem.receiver.address}</p>
+                </div>
               </div>
             </div>
             <div className='flex3-2'>
-              <div className = "flex1">
+              <div className="flex1">
                 <p>THÔNG TIN ĐƠN HÀNG</p>
               </div>
-              <div className = "flex4">
-                <div className = "flex5">
+              <div className="flex4">
+                <div className="flex5">
                   <p className='pLabel'>Mã đơn hàng: </p>
-                  <p>123456789</p>
+                  <p>{packageItem.id}</p>
                   <p></p>
                 </div>
-                <div className = "flex5">
+                <div className="flex5">
                   <p className='pLabel'>Mã người gửi: </p>
-                  <p>123xxxxxx</p>
+                  <p>{packageItem.sender.id}</p>
                   <p></p>
                 </div>
-                <div className = "flex5">
+                <div className="flex5">
                   <p className='pLabel'>Mã người nhận: </p>
-                  <p>321xxxxxx</p>
+                  <p>{packageItem.receiver.id}</p>
                   <p></p>
                 </div>
-                <div className = "flex5">
+                <div className="flex5">
                   <p className='pLabel'>Tên sản phẩm: </p>
-                  <p>Manga</p>
+                  <p>{packageItem.name}</p>
                   <p></p>
                 </div>
-                <div className = "flex5">
+                <div className="flex5">
                   <p className='pLabel'>Giá vận chuyển: </p>
-                  <p>25.000 (VNĐ)</p>
+                  <p>{packageItem.shippingCost}</p>
                   <p></p>
                 </div>
-                <div className = "flex5">
+                <div className="flex5">
                   <p className='pLabel'>Điểm gửi hàng: </p>
-                  <p>Transaction Point Start</p>
+                  <p>{packageItem.transactionPointStart.name}</p>
                   <p></p>
                 </div>
-                <div className = "flex5">
+                <div className="flex5">
                   <p className='pLabel'>Điểm nhận hàng: </p>
-                  <p>Transaction Point End</p>
+                  <p>{packageItem.transactionPointEnd}</p>
                   <p></p>
                 </div>
-                <div className = "flex5">
+                <div className="flex5">
                   <p className='pLabel'>Trạng thái: </p>
-                  <p className='pStatus'>Đang xử lý</p>
+                  <p className='pStatus'>{packageItem.Status.nameOfStatus}</p>
                   <p></p>
                 </div>
               </div>
@@ -121,14 +143,10 @@ export const Home = () => {
             <i>
               <strong>Thời gian hỗ trợ:</strong> Thứ 2 đến thứ 6 (8h30-17h00) hàng tuần</i>
           </p>
+          <img id="tem" src={tem} alt=""></img>
         </div>
       </div>
-
-      {/* {packageInfo && (
-        <div className="packageForm">
-
-        </div>
-      )} */}
+      )}
     </div>
   )
 }
